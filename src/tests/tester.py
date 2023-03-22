596668fb107029952851977980b5770b897778f3
Cristovao Cordeiro <cristovao.cordeiro@canonical.com>
2023-03-22 14:45:49 +0100

    feat: initialize oci-factory
    Initialize the oci-factory repository
    with the base Build, Test, Validate and Release
    pipelines.

---
+++ src/tests/tester.py
@@ -0,0 +1,83 @@
+# pylint: disable=no-name-in-module
+# pylint: disable=no-self-argument
+import docker
+import logging
+import os
+import subprocess
+from typing import Literal
+
+from pydantic import BaseModel, PrivateAttr
+
+
+class TestingError(Exception):
+    """Error when any of the tests fail"""
+
+
+class Test(BaseModel):
+    """Sets the ground for all tests"""
+
+    # Name of the ROCK, or path if provided in OCI formats
+    image: str
+    # Format of the provided image
+    image_format: Literal["docker-daemon", "oci-archive", "fs"]
+    # Constant Docker client
+    _docker_client: object = PrivateAttr()
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self._docker_client = docker.from_env()
+
+        # Validates the image
+        if self.image_format == "docker-daemon":
+            # check that image exists
+            self._docker_client.images.get(self.image)
+        else:
+            self.image = os.path.abspath(self.image)
+
+        if self.image_format == "oci-archive":
+            # then it's a file
+            assert os.path.isfile(
+                self.image
+            ), f"Cannot find OCI archive file {self.image}."
+
+        if self.image_format == "fs":
+            # then it's a directory
+            assert os.path.isdir(
+                self.image
+            ), f"Cannot find image filesystem directory {self.image}."
+
+    def convert(
+        self, origin: str, origin_format: str, dest: str, dest_format: str
+    ) -> None:
+        logging.info(f"Copying from {origin_format}:{origin} to {dest_format}:{dest}")
+        try:
+            if not origin_format.startswith("docker"):
+                mounted_origin = f"/rootfs{os.path.abspath(origin)}"
+            if not dest_format.startswith("docker"):
+                mounted_dest = f"/rootfs{os.path.abspath(dest)}"
+
+            copy_cmd = (
+                f"copy {origin_format}:{mounted_origin} {dest_format}:{mounted_dest}"
+            )
+            skopeo = self._docker_client.containers.run(
+                "quay.io/skopeo/stable:v1.8",
+                working_dir="/rock",
+                volumes={
+                    "/": {"bind": "/rootfs"},
+                    "/var/run/docker.sock": {"bind": "/var/run/docker.sock"},
+                },
+                command=copy_cmd,
+                detach=True,
+            )
+            skopeo.wait()
+            logging.info(skopeo.logs().decode())
+        except Exception as skopeoerror:
+            logging.warning(f"Failed to run Skopeo as a container: {skopeoerror}")
+            logging.info("Attempting to run Skopeo from the underlying system...")
+            copy_cmd = f"skopeo copy {origin_format}:{origin} {dest_format}:{dest}"
+            output = subprocess.check_output(
+                copy_cmd.split((" ")), universal_newlines=True
+            )
+            logging.info(output)
+
+        return dest
---
+++ src/uploads/infer_image_track.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import logging
+import subprocess
+import yaml
+
+logging.basicConfig()
+
+DOCKERFILE_IMAGE_VERSION = os.getenv("DOCKERFILE_IMAGE_VERSION", None)
+
+
+def get_release_from_codename(codename: str) -> str:
+    """Uses distro-info tools to infer the Ubuntu release from its codename."""
+    all_releases = subprocess.check_output(
+        ["ubuntu-distro-info", "--fullname", "--all"], universal_newlines=True
+    ).splitlines()
+
+    return list(filter(lambda x: codename.lower() in x.lower(), all_releases))[
+        0
+    ].split()[1]
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--recipe-dirname",
+    help="Path to the directory where rockcraft.yaml/Dockerfile is",
+    required=True,
+)
+args = parser.parse_args()
+
+if DOCKERFILE_IMAGE_VERSION:
+    with open(f"{args.recipe_dirname.rstrip('/')}/Dockerfile") as dockerfile:
+        dockerfile_content = dockerfile.read().splitlines()
+
+    base = list(filter(lambda x: "FROM" in x, dockerfile_content))[-1]
+
+    try:
+        base_release = float(base.split(":")[-1])
+    except ValueError:
+        logging.warning(
+            f"Could not infer Ubuntu release from {base}. Trying with codename."
+        )
+        base_release = float(get_release_from_codename(base.split(":")[-1]))
+
+    version = DOCKERFILE_IMAGE_VERSION
+else:
+    with open(f"{args.recipe_dirname.rstrip('/')}/rockcraft.yaml") as rockcraft_file:
+        rockcraft_yaml = yaml.safe_load(rockcraft_file)
+
+    rock_base = (
+        rockcraft_yaml["base"]
+        if rockcraft_yaml["base"] != "bare"
+        else rockcraft_yaml["build-base"]
+    )
+
+    try:
+        base_release = float(rock_base.split(":")[-1])
+    except ValueError:
+        logging.warning(
+            f"Could not infer ROCK's base release from {rock_base}. Trying with codename."
+        )
+        base_release = float(get_release_from_codename(rock_base.split(":")[-1]))
+
+    version = rockcraft_yaml["version"]
+
+track = f"{version}-{base_release}"
+print(f"ROCK track: {track}")
+
+with open(os.environ["GITHUB_OUTPUT"], "a") as gh_out:
+    print(f"track={track}", file=gh_out)
