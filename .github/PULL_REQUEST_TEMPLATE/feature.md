fd09e4438739cd6a86e80a8998ae9cef82c22ea3
Cristovao Cordeiro <cristovao.cordeiro@canonical.com>
2023-05-23 10:37:59 +0200

    feat(ci): test mock images and validate support for custom tracks (#19)
    Co-authored-by: github-actions[bot] <rocks-dev@lists.canonical.com>

---
+++ .github/PULL_REQUEST_TEMPLATE/feature.md
@@ -0,0 +1,14 @@
+- [ ] I have signed the [CLA](http://www.ubuntu.com/legal/contributors/)?
+- [ ] I am following the [CONTRIBUTING](../CONTRIBUTING.md) guidelines
+
+---
+
+#### feature
+
+-
+
+
+
+-----
+
+*Picture of a cool ROCK:*
--- src/image/get_canonical_tags_from_swift.sh
+++ src/image/get_canonical_tags_from_swift.sh
@@ -5,8 +5,10 @@ source $(dirname $0)/../configs/swift.public.novarc
 
 set -x
 
-IMAGE_NAME=$(echo ${TRIGGER_PATH} | rev | cut -d '/' -f 2 | rev)
 canonical_tags=$(swift list $SWIFT_CONTAINER_NAME -p $IMAGE_NAME \
     | awk -F '/' '{print $2"_"$3}' | uniq | sort | tr '\n' ',')
 
 echo "canonical-tags=${canonical_tags}" >> "$GITHUB_OUTPUT"
+
+echo "${canonical_tags}" > canonical-tags-file.txt
+echo "canonical-tags-file=canonical-tags-file.txt" >> "$GITHUB_OUTPUT"
