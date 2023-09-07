74a11beb7d69c3588cfb04de59aa3e00b0ba4308
Cristovao Cordeiro <cristovao.cordeiro@canonical.com>
2023-09-07 17:25:26 +0200

    chore: use GH runners' yq installation (#56)

--- src/notifications/requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-YQ_VERSION=${YQ_VERSION:-"v4.32.2"}
-YQ_BINARY=${YQ_BINARY:-"yq_linux_amd64"}
-
-wget -q "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}"
-sudo mv "${YQ_BINARY}" /usr/bin/yq
-sudo chmod +x /usr/bin/yq
