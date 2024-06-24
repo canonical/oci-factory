f697e500fc1f559a4b69055855041995b1752a00
Zhijie Yang <zhijie.yang@canonical.com>
2024-06-24 14:28:50 +0200

    fix(cli-client): fix the code styling accord. to review

--- src/cli-client/internals/cli/cli_main.go
+++ src/cli-client/internals/cli/cli_main.go
@@ -10,14 +10,12 @@ import (
 	"github.com/canonical/oci-factory/cli-client/internals/logger"
 )
 
-// Example: oci-factory upload -y --release tracks=1.0-22.04,risks=edge,eol=2025-06-01
-
 type CmdRelease struct {
-	Revision int `long:"revision" description:"The integer-typed revision number of the image release"`
+	Revision int `long:"revision" description:"The revision number of the image release"`
 }
 
 var opts struct {
-	// Leave `release` here for now. Should be moved into a `cmd_release.go` in phase 2.
+	// TODO: Leave `release` here for now. Should be moved into a `cmd_release.go` in phase 2.
 	CmdRelease CmdRelease `command:"release" description:"Release (re-tag) the image into the container registries"`
 	Confirm    bool       `short:"y" description:"Confirm the upload/release action by default"`
 }
@@ -36,7 +34,7 @@ func blockForConfirm(s string, tries int) {
 	r := bufio.NewReader(os.Stdin)
 
 	for ; tries > 0; tries-- {
-		fmt.Printf("%s [Y/n]: ", s)
+		fmt.Printf("%s [y/N]: ", s)
 
 		res, err := r.ReadString('\n')
 		if err != nil {
@@ -45,11 +43,11 @@ func blockForConfirm(s string, tries int) {
 
 		res = strings.TrimSpace(res)
 
-		if res == "y" || res == "yes" || res == "" {
+		if res == "y" || res == "yes" {
 			return
-		} else if res == "n" || res == "no" {
+		} else if res == "n" || res == "no" || res == "" {
 			fmt.Println("Cancelled")
-			os.Exit(0)
+			os.Exit(1)
 		} else {
 			continue
 		}
--- src/cli-client/internals/cli/cli_upload.go
+++ src/cli-client/internals/cli/cli_upload.go
@@ -19,38 +19,40 @@ type CmdUpload struct {
 	UploadRelease []string `long:"release" description:"Release images to container registries.\nSyntax: --release tracks=<release track>,risks=<risk1>[,<risk2>...],eol=yyyy-mm-dd"`
 }
 
-var cmdUpload CmdUpload
-var releases = make([]UploadRelease, 0)
-
 func init() {
-	// fmt.Println("DEBUG Adding upload to parser commands")
 	parser.Command.AddCommand("upload", "Trigger the build and release for the image in the current working directory",
-		"long description", &cmdUpload)
+		"long description", &CmdUpload{})
 }
 
 func (c *CmdUpload) Execute(args []string) error {
-	// fmt.Println("DEBUG CMD upload args:", args)
-	parseUploadReleases(c.UploadRelease)
-	triggerUploadReleases()
+	releases, err := parseUploadReleases(c.UploadRelease)
+	if err != nil {
+		return err
+	}
+	triggerUploadReleases(releases)
 	return nil
 }
 
-func parseUploadReleases(args []string) error {
+// parseUploadReleases parses the release arguments and returns a list of UploadRelease structs.
+// The release arguments are expected to be in the format "tracks=<track>,risks=<risk1>[,<risk2>...],eol=yyyy-mm-dd".
+// Multiple release arguments can be passed, separated by spaces.
+func parseUploadReleases(args []string) ([]UploadRelease, error) {
 	var release UploadRelease
+	var releases = make([]UploadRelease, 0)
+
 	for _, argStr := range args {
-		// fmt.Println("DEBUG Upload release arg:", argStr)
 		parts := strings.Split(argStr, ",")
 
 		var risks []string
 		for _, part := range parts {
-			keyValue := strings.SplitN(part, "=", 2)
+			keyValue := strings.Split(part, "=")
 			if len(keyValue) == 1 && len(risks) > 0 {
 				// This is part of the risks list (e.g., "stable" in "risks=beta,stable")
 				risks = append(risks, keyValue[0])
 				continue
 			}
 			if len(keyValue) != 2 {
-				return fmt.Errorf("invalid key-value pair: %s", part)
+				return nil, fmt.Errorf("invalid key-value pair: %s", part)
 			}
 			key := keyValue[0]
 			value := keyValue[1]
@@ -63,46 +65,46 @@ func parseUploadReleases(args []string) error {
 			case "eol":
 				release.EndOfLife = value
 			default:
-				fmt.Errorf("unknown key: %s", key)
+				return nil, fmt.Errorf("unknown key: %s", key)
 			}
 		}
 		release.Risks = risks
 		releases = append(releases, release)
 	}
-	return nil
+	return releases, nil
 }
 
-// use the global variable `releases` directly
-func triggerUploadReleases() {
+func triggerUploadReleases(releases []UploadRelease) {
 	// really builds
-	if len(releases) > 0 {
-		buildMetadata := trigger.InferBuildMetadata()
-		var uploadReleaseTrack = make(trigger.UploadReleaseTrack)
-		for _, release := range releases {
-			eol, err := ValidateAndFormatDate(release.EndOfLife)
-			if err != nil {
-				logger.Panicf("EOL with wrong data formats: %v", err)
-			}
-			uploadReleaseTrack[release.Track] = trigger.UploadRelease{
-				EndOfLife: eol,
-				UploadReleaseRisks: trigger.UploadReleaseRisks{
-					Risks: release.Risks,
-				},
-			}
-		}
-		imageTrigger := trigger.NewUploadImageTrigger(buildMetadata, uploadReleaseTrack)
-		uploadTrigger := trigger.NewUploadTrigger([]trigger.UploadImageTrigger{imageTrigger})
-		imageName := trigger.GetRockcraftImageName()
-		payload := client.NewPayload(imageName, uploadTrigger.ToYamlString())
-		fmt.Printf("The %s image will be built and released with following triggers:\n", imageName)
-		fmt.Println(uploadTrigger.ToYamlString())
-		if !opts.Confirm {
-			blockForConfirm("Do you want to continue?", 2)
-		}
-		externalRefID := payload.Inputs.ExternalRefID
-		client.DispatchWorkflow(payload)
-		client.WorkflowPolling(externalRefID)
-	} else {
-		fmt.Println("Nothing passed in --release, upload will be skipped.")
+	if len(releases) == 0 {
+		fmt.Println("No release track specified, no build will be triggered.")
+		return
 	}
+
+	buildMetadata := trigger.InferBuildMetadata()
+	var uploadReleaseTrack = make(trigger.UploadReleaseTrack)
+	for _, release := range releases {
+		eol, err := ValidateAndFormatDate(release.EndOfLife)
+		if err != nil {
+			logger.Panicf("EOL with wrong data formats: %v", err)
+		}
+		uploadReleaseTrack[release.Track] = trigger.UploadRelease{
+			EndOfLife: eol,
+			UploadReleaseRisks: trigger.UploadReleaseRisks{
+				Risks: release.Risks,
+			},
+		}
+	}
+	imageTrigger := trigger.NewUploadImageTrigger(buildMetadata, uploadReleaseTrack)
+	uploadTrigger := trigger.NewUploadTrigger([]trigger.UploadImageTrigger{imageTrigger})
+	imageName := trigger.GetRockcraftImageName()
+	payload := client.NewPayload(imageName, uploadTrigger.ToYamlString())
+	fmt.Printf("The %s image will be built and released with following triggers:\n", imageName)
+	fmt.Println(uploadTrigger.ToYamlString())
+	if !opts.Confirm {
+		blockForConfirm("Do you want to continue?", 2)
+	}
+	externalRefID := payload.Inputs.ExternalRefID
+	client.DispatchWorkflow(payload)
+	client.WorkflowPolling(externalRefID)
 }
