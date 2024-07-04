ce8d44ee92536294138a8b2429505a387979f349
Zhijie Yang <zhijie.yang@canonical.com>
2024-07-04 10:03:34 +0200

    feat(temporal): make mm settings all secrets

--- src/workflow-engine/charms/temporal-worker/oci_factory/notification/mattermost_notifier.py
+++ src/workflow-engine/charms/temporal-worker/oci_factory/notification/mattermost_notifier.py
@@ -2,9 +2,12 @@ import json
 import logging
 import os
 import requests
+import sys
 import time
 from typing import Optional
 
+
+logging.basicConfig(level=os.environ.get("TWC_LOG_LEVEL", "info").upper())
 
 COLORS = {
     "success": "#33CC33",
@@ -12,22 +15,31 @@ COLORS = {
     "unknown": "#B0B0B0",
 }
 
-channel_id = os.environ.get(
-    "MATTERMOST_CHANNEL_ID", "yax1c3pmm7natd5ebw3cngyrpw"
-)  # `ROCKS Bot Test` by default
-mattermost_server = "https://chat.canonical.com"
-posts_url = f"{mattermost_server}/api/v4/posts"
-headers = {
-    "Authorization": f"Bearer {os.environ['MATTERMOST_TOKEN']}",
+REQUIRED_ENVS = ["MATTERMOST_CHANNEL_ID", "MATTERMOST_TOKEN", "MATTERMOST_SERVER"]
+
+missing_envs = [env for env in REQUIRED_ENVS if os.environ.get(env) is None]
+if missing_envs:
+    logging.error(
+        f"Unable to notify on Mattermost, missing environment variables: {missing_envs}"
+    )
+    sys.exit(1)
+
+MATTERMOST_CHANNEL_ID = os.environ.get("MATTERMOST_CHANNEL_ID")
+MATTERMOST_TOKEN = os.environ.get("MATTERMOST_TOKEN")
+MATTERMOST_SERVER = os.environ.get("MATTERMOST_SERVER")
+
+POST_URL = f"{MATTERMOST_SERVER}/api/v4/posts"
+PATCH_URL = POST_URL + "/{}/patch"
+
+HEADERS = {
+    "Authorization": f"Bearer {MATTERMOST_TOKEN}",
     "Content-Type": "application/json",
 }
-
-logging.basicConfig(level=os.environ.get("TWC_LOG_LEVEL", "info").upper())
 
 
 def send_message(title: str, message: str) -> str:
     payload = {
-        "channel_id": channel_id,
+        "channel_id": MATTERMOST_CHANNEL_ID,
         "props": {
             "attachments": [
                 {
@@ -45,7 +57,7 @@ def send_message(title: str, message: str) -> str:
         logging.warning("Unable to notify on Mattermost, MATTERMOST_TOKEN is not set!")
 
     data = json.dumps(payload).encode("utf-8")
-    res = requests.post(posts_url, headers=headers, data=data, timeout=10)
+    res = requests.post(POST_URL, headers=HEADERS, data=data, timeout=10)
     res.raise_for_status()
     return res.json()["id"]
 
@@ -55,11 +67,8 @@ def update_status_and_message(
     message_id: str, success: bool, message: Optional[str] = None
 ) -> None:
     post_res = requests.get(
-        f"{posts_url}/{message_id}",
-        headers={
-            "Authorization": f"Bearer {os.environ['MATTERMOST_TOKEN']}",
-            "Content-Type": "application/json",
-        },
+        f"{POST_URL}/{message_id}",
+        headers=HEADERS,
         timeout=10,
     )
     post_res.raise_for_status()
@@ -80,7 +89,7 @@ def update_status_and_message(
 
     data = json.dumps(payload).encode("utf-8")
     res = requests.put(
-        f"{posts_url}/{message_id}/patch", headers=headers, data=data, timeout=10
+        PATCH_URL.format(message_id), headers=HEADERS, data=data, timeout=10
     )
     res.raise_for_status()
 
@@ -90,10 +99,6 @@ if __name__ == "__main__":
     title = "[OCI Factory Temporal Worker] Image Rebuild on New Ubuntu Release"
     id = send_message(title, message)
     time.sleep(5)
-    update_status_and_message(
-        id, 0, message="**Release**: 100.04\n**Status**: Failed"
-    )
+    update_status_and_message(id, 0, message="**Release**: 100.04\n**Status**: Failed")
     time.sleep(5)
-    update_status_and_message(
-        id, 1, message="**Release**: 100.04\n**Status**: Success"
-    )
+    update_status_and_message(id, 1, message="**Release**: 100.04\n**Status**: Success")
