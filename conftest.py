b220f8be9cdfe2090d7f657714e04138429e09e2
Adrian Clay Lake <clay.lake@canonical.com>
2024-09-03 10:23:44 +0200

    refactor(conftest.py): missing newline

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
+
 def is_symlink_dir(path):
     """Returns true if path is a symlink to a directory"""
     path_obj = Path(path)
     result = path_obj.is_dir() and path_obj.is_symlink()
-    
+
     return result
+
 
 def pytest_ignore_collect(path, config):
     """Since we use symlinks to ease imports from shared dir
@@ -13,4 +15,4 @@ def pytest_ignore_collect(path, config):
     return true in this function will be skipped."""
 
     skip_path = is_symlink_dir(path)
-    return skip_path 
+    return skip_path
