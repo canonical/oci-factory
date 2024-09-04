d33f9a0239077822fe033f81c2d5503422cbf5d9
Adrian Clay Lake <clay.lake@canonical.com>
2024-09-04 16:48:23 +0200

    refactor: removed files from dangling commits

--- tests/fixtures/buffers.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from io import StringIO
-
-
-@pytest.fixture
-def str_buff():
-    """String IO fixture for simulating a file object"""
-    with StringIO() as buffer:
-        yield buffer
