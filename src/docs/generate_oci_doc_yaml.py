4d1fae1b3f426066c8dc9286c6d946679600f44a
Anas Husseini <anas.husseini@canonical.com>
2024-11-14 10:30:20 +0200

    fix a typo in comparison

--- src/docs/generate_oci_doc_yaml.py
+++ src/docs/generate_oci_doc_yaml.py
@@ -268,7 +268,7 @@ class OCIDocumentationData:
                 eol = parser.parse(all_tracks[track_base])
                 release_data["support"] = {"until": eol.strftime("%m/%Y")}
 
-                if eol > datetime.now(timezone.utc):
+                if eol < datetime.now(timezone.utc):
                     release_data["deprecated"] = {
                         "date": eol.strftime("%m/%Y")
                     }
