80e71fb2a696c11f3d9e46f800478daf82f16962
Zhijie Yang <zhijie.yang@canonical.com>
2025-02-20 16:37:09 +0100

    feat: use heartbeat

--- tools/workflow-engine/charms/temporal-worker/oci_factory/activities/activity_consume_events.py
+++ tools/workflow-engine/charms/temporal-worker/oci_factory/activities/activity_consume_events.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import subprocess
-from datetime import datetime
+from datetime import datetime, timezone
 
 from confluent_kafka import Consumer, KafkaException
 from temporalio import activity
@@ -30,8 +30,9 @@ logging.basicConfig(level=os.environ.get("TWC_LOG_LEVEL", "info").upper())
 # Custom log format function
 class CustomFormatter(logging.Formatter):
     def format(self, record):
-        timestamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-        return f"{record.filename}: {timestamp}: {record.getMessage()}"
+        # timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S:%MSZ")
+        # return f"{record.filename}: {timestamp}: {record.getMessage()}"
+        return f"{record.filename}: {record.getMessage()}"
 
 # Configure the root logger
 logging.basicConfig(
@@ -55,7 +56,9 @@ async def consume(topic: str, consumer_group: str) -> dict:
         f"Consuming messages from topic {topic} "
         f"with consumer group {consumer_group}"
     )
-
+    activity.heartbeat(f"Consuming messages from topic {topic} "
+        f"with consumer group {consumer_group}"
+    )
     activity_info = activity.info()
     activity_info_formatter = (activity_info.workflow_id, activity_info.workflow_run_id)
 
@@ -72,11 +75,12 @@ async def consume(topic: str, consumer_group: str) -> dict:
         deserialize = registry.topic_deserializer(topic)
         consumer = Consumer(consumer_config)
         consumer.subscribe([topic])
+        activity.heartbeat(f"Subscribed to topic {topic}")
         try:
             while True:
-                logging.info("Waiting for message from event bus.")
+                logging.info(f"Waiting for message from event bus on topic {topic}")
                 msg = consumer.poll(timeout=300.0)
-
+                activity.heartbeat(f"Polling timed out from topic {topic}")
                 if msg is None:
                     continue
                 logging.info(f"Message received: {msg}")
@@ -87,6 +91,7 @@ async def consume(topic: str, consumer_group: str) -> dict:
         finally:
             consumer.close()
 
+    activity.heartbeat("Sending message to Mattermost")
     logging.info("Release: {}".format(value["release"]))
     message_id = send_message(
         MM_MESSAGE_TITLE,
@@ -97,20 +102,20 @@ async def consume(topic: str, consumer_group: str) -> dict:
     # Details see ROCKS-1197
     curr_file_path = os.path.dirname(os.path.realpath(__file__))
     script_full_path = os.path.join(curr_file_path, "find_images_to_update.py")
-
+    activity.heartbeat(f"Triggering rebuild for release {value['release']}")
     proc = subprocess.Popen(
         ["python3", script_full_path, "{}".format(value.get("release"))],
         stderr=subprocess.PIPE,
     )
     success = proc.wait() == 0
-
+    activity.heartbeat("Updating Mattermost message")
     status = "Success" if success else "Failed"
     update_status_and_message(
         message_id,
         success,
         MM_MESSAGE_BODY.format(value["release"], status, *activity_info_formatter),
     )
-
+    activity.heartbeat("Consumer activity completed")
     return {
         "eventbus_message": value,
         "find_images_to_update.py": proc.stderr.read().decode("utf-8"),
